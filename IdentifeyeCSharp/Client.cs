ec04394e48320155aa80275e7be30b9e0757e3f8
ZakFahey <z.fahey@gmail.com>
2019-02-11 12:25:05 -0500

    Moved all the other parts of this project into this repository

---
+++ IdentifeyeCSharp/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Identifeye;
+using Grpc.Core;
+using System.Collections.Generic;
+
+namespace IdentifeyeCSharp
+{
+    public class Client
+    {
+        private Channel channel;
+        private IdentifeyeService.IdentifeyeServiceClient client;
+
+        public Client(string target)
+        {
+            channel = new Channel(target, ChannelCredentials.Insecure);
+            client = new IdentifeyeService.IdentifeyeServiceClient(channel);
+        }
+
+        public bool SendData(DataPoint data)
+        {
+            return client.SendData(data).Success;
+        }
+
+        public bool SendDataBulk(IList<DataPoint> data)
+        {
+            return client.SendDataBulk(new BulkData { Data = { data } }).Success;
+        }
+
+        public void Stop()
+        {
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Identifeye;
+using System.Threading;
+using IdentifeyeCSharp;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            var client = new Client("127.0.0.1:50051");
+            Console.WriteLine("Started pinging");
+            while(true)
+            {
+                client.SendData(new DataPoint
+                {
+                    AccountNameHash = "abc",
+                    ActivePlaytime = 10,
+                    CharacterNameHash = "def",
+                    IpHash = "hij",
+                    IpLocationHash = "klm",
+                    IsBanned = false
+                });
+                Console.WriteLine("Sent data");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
